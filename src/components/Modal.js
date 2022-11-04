9b7bd579156b1a4ae01973f2830356f3f7e4a769
Charles Ozochukwu <charlesozochukwu2004@gmail.com>
2022-11-04 22:44:49 +0100

    changed it back to h4

--- src/components/Modal.js
+++ src/components/Modal.js
@@ -14,7 +14,7 @@ const Modal = () => {
       <div className="modal-container">
         <img src={image} alt={title} className="img modal-img" />
         <div className="modal-content">
-          <h3>{title}</h3>
+          <h4>{title}</h4>
           <p>Cooking Instructions</p>
           <p>{text}</p>
           <a href={source} target ="_blank">Original source</a>
